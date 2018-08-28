3714b6ca9de14686231391afeeb254b3e00c3e4d
MuthuMaha <muthumaharajan1992@gmail.com>
2018-08-28 14:24:40 +0530

    Route moved

--- routes/api.php
+++ routes/api.php
@@ -52,20 +52,21 @@ use Illuminate\Http\Request;
 		Route::get('query_response', 'ExamController@getqueryResponse');
 		Route::put('query_response', 'ExamController@updatequeryResponse');
 		Route::delete('query_response', 'ExamController@deleteResponse');
+		
+		//let these be here for somtime
+		Route::get('groups','BaseController@groups');
+		Route::get('class_years/{group_id}','BaseController@class_year_wrt_group');
+		Route::get('streams/{group_id}/{class_id}','BaseController@stream_wrt_group_class_year');
+		Route::get('programs/{stream_id}/{class_id}','BaseController@programs_wrt_stream_class_year');
+
+
+		//student Login Api's 
+		Route::get('student/{stud_id}/profile','StudentController@profile_details');
+		Route::get('student/{stud_id}/reports_card','StudentController@written_tests');
 	});
 
 
 
-//let these be here for somtime
-   Route::get('groups','BaseController@groups');
-   Route::get('class_years/{group_id}','BaseController@class_year_wrt_group');
-   Route::get('streams/{group_id}/{class_id}','BaseController@stream_wrt_group_class_year');
-   Route::get('programs/{stream_id}/{class_id}','BaseController@programs_wrt_stream_class_year');
-
-
-//student Login Api's 
-Route::get('student/{stud_id}/profile','StudentController@profile_details');
-Route::get('student/{stud_id}/reports_card','StudentController@written_tests');
 
 
 	
